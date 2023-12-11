284545e01454e9add04cc00547d8daf96466ed44
mohitlekh <workmohitlekhrjani>
2023-12-12 01:08:16 +0530

    Upgrade dummy1

--- src/App.js
+++ src/App.js
@@ -30,11 +30,8 @@ function App() {
 				<Route path="/articles" element={<Articles />} />
 				<Route path="/article/:slug" element={<ReadArticle />} />
 				<Route path="/contact" element={<Contact />} />
-				<Route
-					path="/dummy"
-					Component={() => <>Dummy route for testing</>}
-				/>
-				<Route path="/resume" Component={() => <Resume />} />
+				<Route path="dummy" element={<>Dummy route for testing</>} />
+				<Route path="resume" element={<Resume />} />
 				<Route path="*" element={<Notfound />} />
 			</Routes>
 		</div>
